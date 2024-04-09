526b3792980ca45cebb79e02499f2ae94a0b669b
stray-code <abc1007020@gmail.com>
2024-04-09 23:48:18 +0900

    crxjs追加と設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: '画像ダウンロード',
+  description: 'サイトの画像を一括でダウンロードできます。',
+  version: '1.0.0',
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+});
