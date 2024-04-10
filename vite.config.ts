b472d77bcf473519dcbdecf7821a124b42430c6a
stray-code <abc1007020@gmail.com>
2024-04-10 21:43:56 +0900

    manifest修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { crx, defineManifest } from '@crxjs/vite-plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: '画像ダウンロード',
-  description: 'サイトの画像を一括でダウンロードできます。',
-  version: '1.0.0',
+  name: "画像ダウンロード",
+  description: "サイトの画像を一括でダウンロードできます。",
+  version: "1.0.0",
+  icons: {
+    16: "img/icon16.png",
+    48: "img/icon48.png",
+    128: "img/icon128.png",
+  },
+  action: {
+    default_icon: "img/icon16.png",
+    default_popup: "src/popup/index.html",
+  },
+  content_scripts: [
+    {
+      js: ["src/content/main.ts"],
+      matches: ["http://*/*", "https://*/*"],
+    },
+  ],
+  side_panel: {
+    default_path: "src/sidePanel/index.html",
+  },
+  permissions: ["sidePanel"],
 });
 
 // https://vitejs.dev/config/
