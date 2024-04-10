06e35005e168342e61fb3bc142e2b5693850c73b
stray-code <abc1007020@gmail.com>
2024-04-10 22:05:57 +0900

    相対パスに対応

--- src/content/main.ts
+++ src/content/main.ts
@@ -2,7 +2,8 @@ chrome.runtime.onMessage.addListener(
   (message, _, sendResponse: (response: (string | null)[]) => void) => {
     if (message.type === "GET_IMAGE_URLS") {
       const elements = document.querySelectorAll("img");
-      const urls = [...elements].map((element) => element.getAttribute("src"));
+      // 相対パスでも取得できるように
+      const urls = [...elements].map((element) => element.currentSrc);
       sendResponse(urls);
 
       return;
